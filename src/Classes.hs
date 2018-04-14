e65d6568fe98517e4dd9be5e85957bd3adea29e5
Chris Penner <christopher.penner@gmail.com>
2018-04-14 15:51:16 -0600

    Using simple IO

--- src/Classes.hs
+++ src/Classes.hs
@@ -9,19 +9,19 @@ import Data.Time
 
 -- type SendEvent = forall a b m. a -> m b
 -- data Events m a = Events (ContT () m a)
-type Event m a = m a
+type Event m a = ContT () m a
 type Behaviour m a = m a
 
-eventStream :: (MonadIO m, MonadCont m) => ((a -> m b) -> m a) -> Event m a
-eventStream = callCC
+eventStream :: ((a -> m ()) -> m ()) -> Event m a
+eventStream eventSpawner = ContT eventSpawner
 
-lineEvent :: (MonadIO m, MonadCont m) => Event m String
+lineEvent :: Event IO String
 lineEvent = eventStream allLines
 
-allLines :: MonadIO m => (String -> m a) -> m a
-allLines sendEvent = forever (liftIO getLine >>= sendEvent)
+allLines :: (String -> IO ()) -> IO ()
+allLines sendEvent = forever (getLine >>= sendEvent)
 
-prog :: (MonadCont m, MonadIO m) => Event m ()
+prog :: Event IO ()
 prog = do
   a <- lineEvent *> time
   liftIO $ print a
