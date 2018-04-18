1e19c170a65137c846b98fe392397cd2d592b9d0
Chris Penner <christopher.penner@gmail.com>
2018-04-17 19:22:39 -0600

    Use TVars for tracking state

--- src/Trans.hs
+++ src/Trans.hs
@@ -6,6 +6,7 @@
 
 module Trans where
 
+import Control.Applicative
 import Control.Concurrent.Async
 import Control.Concurrent.STM
 import Control.Monad.Reader
@@ -19,69 +20,81 @@ instance Functor Event where
   fmap f (Event t g) = Event t (f . g)
 
 data Trigger a = Trigger
-  { fire :: (a -> STM ())
+  { fire :: (a -> IO ())
+  }
+
+data NetworkState = NetworkState
+  { signalExit :: IO ()
+  , jobs :: TVar [Async ()]
   }
 
 newtype Network m a =
-  Network (ReaderT (IO ()) (StateT [Async ()] m) a)
-  deriving ( Functor
-           , Applicative
-           , Monad
-           , MonadReader (IO ())
-           , MonadState [Async ()]
-           )
+  Network (ReaderT NetworkState m a)
+  deriving (Functor, Applicative, Monad, MonadReader NetworkState)
 
 deriving instance MonadIO m => MonadIO (Network m)
 
-newEvent :: STM (Event a, Trigger a)
-newEvent = do
-  broadcastChannel <- newBroadcastTChan
-  let evt = Event broadcastChannel id
-  return (evt, Trigger (writeTChan broadcastChannel))
+newEvent :: MonadIO m => Network m (Event a, Trigger a)
+newEvent =
+  liftIO . atomically $ do
+    broadcastChannel <- newBroadcastTChan
+    let evt = Event broadcastChannel id
+    return (evt, Trigger (atomically . writeTChan broadcastChannel))
 
-respond :: MonadIO m => Event a -> (a -> IO ()) -> Network m ()
-respond (Event bChan t) f = do
-  w <- liftIO . async $ handler
-  modify (w :)
+eventFromTrigger :: MonadIO m => ((a -> IO ()) -> IO ()) -> Network m (Event a)
+eventFromTrigger handler = do
+  (evt, trigger) <- newEvent
+  runJob $ handler (fire trigger)
+  return evt
+
+runJob :: MonadIO m => IO () -> Network m ()
+runJob job = do
+  jobVar <- asks jobs
+  liftIO $ do
+    w <- async job
+    atomically $ modifyTVar jobVar (w :)
+
+react :: MonadIO m => Event a -> (a -> Network IO ()) -> Network m ()
+react (Event bChan t) f = do
+  networkState <- ask
+  runJob $ handler networkState
   where
-    handler = do
+    handler networkState = do
       freshEventChannel <- liftIO . atomically $ dupTChan bChan
       liftIO . forever $ do
         a <- atomically $ readTChan freshEventChannel
-        f (t a)
+        runSimple networkState $ f (t a)
 
 linesEvent :: MonadIO m => Network m (Event String)
-linesEvent = do
-  (evt, trigger) <- liftIO . atomically $ newEvent
-  w <- liftIO . async $ getLines trigger
-  modify (w :)
-  return evt
-  where
-    getLines trigger =
-      forever $ do
-        ln <- getLine
-        atomically $ fire trigger ln
+linesEvent =
+  eventFromTrigger $ \trig ->
+    forever $ do
+      ln <- getLine
+      trig ln
 
 network :: MonadIO m => Network m ()
 network = do
   evt <- linesEvent
   let numbers = show . length <$> evt
-  exitIO <- ask
-  respond evt $ \case
-    ('q':_) -> exitIO >> liftIO (print "exiting?")
+  react evt $ \case
+    ('q':_) -> liftIO (print "exiting") >> exit
     l -> liftIO $ print l
-  respond numbers print
+  react numbers (liftIO . print)
 
 exit :: MonadIO m => Network m ()
-exit = ask >>= liftIO >> liftIO (print "EXITED")
+exit = asks signalExit >>= liftIO
+
+runSimple :: MonadIO m => NetworkState -> Network m () -> m ()
+runSimple exitIO (Network m) = flip runReaderT exitIO $ m
 
 runNetwork :: MonadIO m => Network m () -> m ()
-runNetwork (Network m) = do
-  waitExit <- liftIO $ newTVarIO False
-  jobs <- flip execStateT [] . flip runReaderT (exitIO waitExit) $ m
-  liftIO $ print "PAST!"
-  liftIO (atomically $ readTVar waitExit >>= check)
-  liftIO $ mapM_ cancel jobs
+runNetwork m = do
+  (jobListVar, exitVar) <- liftIO $ liftA2 (,) (newTVarIO []) (newTVarIO False)
+  let initialNetworkState =
+        NetworkState {signalExit = exitIO exitVar, jobs = jobListVar}
+  runSimple initialNetworkState $ m
+  liftIO . atomically $ (readTVar exitVar >>= check)
+  liftIO $ readTVarIO jobListVar >>= mapM_ cancel
   where
     exitIO :: TVar Bool -> IO ()
     exitIO exitVar = atomically $ writeTVar exitVar True
