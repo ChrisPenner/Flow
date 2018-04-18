15fbfcbb12991a033aec416f82217d0ebacbbc63
Chris Penner <christopher.penner@gmail.com>
2018-04-17 22:52:42 -0600

    Add simple combinators

--- src/Trans.hs
+++ src/Trans.hs
@@ -10,7 +10,6 @@ import Control.Concurrent.Async
 import Control.Concurrent.STM
 import Control.Exception
 import Control.Monad.State
-import Data.Time
 
 data Event a =
   forall b. Event (TChan b)
@@ -38,14 +37,39 @@ newtype Network m a =
 
 deriving instance MonadIO m => MonadIO (Network m)
 
+hold :: MonadIO m => a -> Event a -> Network m (Behaviour m a)
+hold initial evt = do
+  holdVar <- liftIO $ newTVarIO initial
+  react evt $ \e -> liftIO . atomically $ writeTVar holdVar e
+  return (Behaviour (liftIO $ readTVarIO holdVar))
+
+mergeMap ::
+     (Foldable f, MonadIO m) => Event a -> (a -> f b) -> Network m (Event b)
+mergeMap evt f = cloneWith evt go
+  where
+    go value trigger = do
+      mapM_ trigger (f value)
+
 pair :: MonadIO m => Event a -> Behaviour m b -> Network m (Event b)
 pair evt (Behaviour samp) = mapEventM evt (const samp)
 
-mapEventM :: MonadIO m => Event a -> (a -> m b) -> Network m (Event b)
-mapEventM evt f = do
+firstE :: MonadIO m => Event a -> Network m (Event a)
+firstE evt = cloneWith evt go
+  where
+    go value trigger = trigger value >> die
+
+die :: MonadIO m => m ()
+die = throw ThreadKilled
+
+cloneWith ::
+     MonadIO m => Event a -> (a -> (b -> m ()) -> m ()) -> Network m (Event b)
+cloneWith evt f = do
   (freshEvt, freshTrig) <- newEvent
-  react evt $ \e -> f e >>= fire freshTrig
+  react evt $ \e -> f e (fire freshTrig)
   return freshEvt
+
+mapEventM :: MonadIO m => Event a -> (a -> m b) -> Network m (Event b)
+mapEventM evt f = cloneWith evt (\value trigger -> f value >>= trigger)
 
 newEvent :: MonadIO m => Network m (Event a, Trigger m a)
 newEvent =
@@ -84,14 +108,14 @@ network :: Network IO ()
 network = do
   exit <- gets signalExit
   evt <- linesEvent
-  let numbers = show . length <$> evt
-      timeBehaviour = Behaviour getCurrentTime
-  timeEvents <- pair evt timeBehaviour
-  react timeEvents print
+  chars <- mergeMap evt id
+  firstChar <- firstE chars
+  -- timeEvents <- pair evt timeBehaviour
+  -- react timeEvents print
   react evt $ \case
     ('q':_) -> liftIO (print "exiting") >> exit
     l -> liftIO $ print l
-  react numbers (liftIO . print)
+  react firstChar (print . (: ": was pressed"))
 
 runSimple :: MonadIO m => NetworkState m -> Network m () -> m (NetworkState m)
 runSimple netState (Network m) = flip execStateT netState $ m
