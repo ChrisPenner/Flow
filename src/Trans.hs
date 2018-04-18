15ae8bba0e1e5a96413f9fc52c515ddfead9a531
Chris Penner <christopher.penner@gmail.com>
2018-04-17 18:38:39 -0600

    Abstract monad underlying Network

--- src/Trans.hs
+++ src/Trans.hs
@@ -1,23 +1,37 @@
-{-# language DeriveFunctor #-}
-{-# language ExistentialQuantification #-}
-{-# language LambdaCase #-}
-{-# language GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 module Trans where
 
-import Control.Concurrent.STM
 import Control.Concurrent.Async
-import Control.Monad.State
+import Control.Concurrent.STM
 import Control.Monad.Reader
+import Control.Monad.State
 
-data Event a = forall b. Event (TChan b) (b -> a)
+data Event a =
+  forall b. Event (TChan b)
+                  (b -> a)
 
 instance Functor Event where
   fmap f (Event t g) = Event t (f . g)
 
-data Trigger a = Trigger (a -> STM ())
+data Trigger a = Trigger
+  { fire :: (a -> STM ())
+  }
 
-newtype Network a = Network (ReaderT (IO ()) (StateT [Async ()] IO) a)
-  deriving (Functor, Applicative, Monad, MonadReader (IO ()), MonadState [Async ()], MonadIO)
+newtype Network m a =
+  Network (ReaderT (IO ()) (StateT [Async ()] m) a)
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MonadReader (IO ())
+           , MonadState [Async ()]
+           )
+
+deriving instance MonadIO m => MonadIO (Network m)
 
 newEvent :: STM (Event a, Trigger a)
 newEvent = do
@@ -25,32 +39,30 @@ newEvent = do
   let evt = Event broadcastChannel id
   return (evt, Trigger (writeTChan broadcastChannel))
 
-fire :: Trigger a -> a -> STM ()
-fire (Trigger f) = f
-
-respond :: Event a -> (a -> IO ()) -> Network ()
+respond :: MonadIO m => Event a -> (a -> IO ()) -> Network m ()
 respond (Event bChan t) f = do
   w <- liftIO . async $ handler
-  modify (w:)
-    where
-      handler = do
-        freshEventChannel <- liftIO . atomically $ dupTChan bChan
-        liftIO . forever $ do
-          a <- atomically $ readTChan freshEventChannel
-          f (t a)
+  modify (w :)
+  where
+    handler = do
+      freshEventChannel <- liftIO . atomically $ dupTChan bChan
+      liftIO . forever $ do
+        a <- atomically $ readTChan freshEventChannel
+        f (t a)
 
-linesEvent :: Network (Event String)
+linesEvent :: MonadIO m => Network m (Event String)
 linesEvent = do
   (evt, trigger) <- liftIO . atomically $ newEvent
   w <- liftIO . async $ getLines trigger
-  modify (w:)
+  modify (w :)
   return evt
-    where
-      getLines trigger = forever $ do
+  where
+    getLines trigger =
+      forever $ do
         ln <- getLine
         atomically $ fire trigger ln
 
-network :: Network ()
+network :: MonadIO m => Network m ()
 network = do
   evt <- linesEvent
   let numbers = show . length <$> evt
@@ -60,16 +72,16 @@ network = do
     l -> liftIO $ print l
   respond numbers print
 
-exit :: Network ()
+exit :: MonadIO m => Network m ()
 exit = ask >>= liftIO >> liftIO (print "EXITED")
 
-runNetwork :: Network () -> IO ()
+runNetwork :: MonadIO m => Network m () -> m ()
 runNetwork (Network m) = do
-  waitExit <- newTVarIO False
+  waitExit <- liftIO $ newTVarIO False
   jobs <- flip execStateT [] . flip runReaderT (exitIO waitExit) $ m
-  print "PAST!"
-  atomically $ readTVar waitExit >>= check
-  mapM_ cancel jobs
-    where
-      exitIO :: TVar Bool -> IO ()
-      exitIO exitVar = atomically $ writeTVar exitVar True
+  liftIO $ print "PAST!"
+  liftIO (atomically $ readTVar waitExit >>= check)
+  liftIO $ mapM_ cancel jobs
+  where
+    exitIO :: TVar Bool -> IO ()
+    exitIO exitVar = atomically $ writeTVar exitVar True
